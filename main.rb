4794ce941d3b40adf339b8c2d3fd27ad90434ad9
Ricardo <ricardomg92@hotmail.com>
2022-01-25 21:55:08 +0100

    Added a basic Computer class

--- main.rb
+++ main.rb
@@ -1,15 +1,28 @@
 # frozen_string_literal: true
 
-# Creates a new player, role can be creator or guesser
+# Creates a new human player, role can be creator or guesser
 class Player
   attr_accessor :name
   attr_reader :role
 
   def initialize(name)
     @name = name
-    @role = 'guesser'
+    @role = 'Guesser'
   end
 end
 
+# Creates a new computer player, role can be creator or guesser
+class Computer
+  attr_reader :role
+
+  def initialize
+    @role = 'Creator'
+  end
+end
+
+# Temporary code for tests
+puts 'Insert human name'
 player = Player.new(gets.chomp)
-puts player.name, player.role
+puts "Human name is #{player.name} and his role is #{player.role}"
+computer = Computer.new
+puts "Computer role is #{computer.role}"
